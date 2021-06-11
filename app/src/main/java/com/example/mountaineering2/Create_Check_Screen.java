49681f1d2ad83f8703d5d141cd1e61bdd178fac0
Joe <joe95223@gamil.com>
2021-06-11 12:16:49 +0800

    2021/06/11 Update 12:16

---
+++ app/src/main/java/com/example/mountaineering2/Create_Check_Screen.java
@@ -0,0 +1,57 @@
+package com.example.mountaineering2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class Create_Check_Screen extends AppCompatActivity {
+    TextView dateShow, mountainShow, peopleShow, sayShow;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_check_screen);
+
+        dateShow = findViewById(R.id.dateCheck);
+        mountainShow = findViewById(R.id.mountainShow);
+        peopleShow = findViewById(R.id.peopleShow);
+        sayShow = findViewById(R.id.sayShow);
+
+        showResult();
+
+    }
+
+
+
+    private void showResult() {
+        Bundle bundle = getIntent().getExtras();
+        String date = bundle.getString("date");
+        String mountain = bundle.getString("mountain");
+        String people = bundle.getString("people");
+        String say = bundle.getString("sayText");
+
+        dateShow.setText(date);
+        mountainShow.setText(mountain);
+        peopleShow.setText(people);
+        sayShow.setText(say);
+    }
+    public void backToCreate(View view) {
+        Intent intent = new Intent();
+        intent.setClass(Create_Check_Screen.this, Create_Group_Screen.class);
+        startActivity(intent);
+        Toast.makeText(Create_Check_Screen.this, "取消創建，重新填寫資料!",  Toast.LENGTH_SHORT).show();
+    }
+    public void goToGroupIngList(View view) {
+        Intent intent = new Intent();
+        intent.setClass(Create_Check_Screen.this, Search_Ing_Group_Screen.class);
+        startActivity(intent);
+        Toast.makeText(Create_Check_Screen.this, "創建成功，前往揪團頁面執行任務吧!",  Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/mountaineering2/sign_in.java
+++ app/src/main/java/com/example/mountaineering2/sign_in.java
@@ -1,10 +1,13 @@
 package com.example.mountaineering2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,36 +15,35 @@ import com.android.volley.RequestQueue;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class sign_in extends AppCompatActivity {
+
     private TextView registered,forgetPassWord;
     private Button loginIn;
+    //----------------宣告帳號密碼---------
+    final String user = "root";
+    final String pass = "123";
+    //----------------------------------
 
     private TextInputLayout Account,passWord;
-
-    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
 
-        Account=findViewById(R.id.Account);
-        passWord=findViewById(R.id.passWord);
+        Account=(TextInputLayout)findViewById(R.id.Account);
+        passWord=(TextInputLayout)findViewById(R.id.passWord);
 
 
         registered=(TextView)findViewById(R.id.Registered);
         registered.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openRegistered();
-            }
+            public void onClick(View v) { openRegistered();}
         });
 
         loginIn=(Button)findViewById(R.id.loginIn);
         loginIn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openHomeScreen();
-            }
+            public void onClick(View v) {judgment();}
         });
 
         forgetPassWord=(TextView)findViewById(R.id.forgetPassWord);
@@ -63,5 +65,25 @@ public class sign_in extends AppCompatActivity {
     public void openforgetPage(){
         Intent intent=new Intent(this,forgetPage.class);
         startActivity(intent);
+    }
+    //-------判斷帳號密碼
+    public void judgment(){
+        String AT="";
+        AT=Account.getEditText().getText().toString();
+        String PW="";
+        PW=passWord.getEditText().getText().toString();
+        if(AT.equals(user)&PW.equals(pass)){
+            //成功
+            openHomeScreen();
+        }else {
+            //失敗
+        new AlertDialog.Builder(sign_in.this).setTitle("錯誤").setMessage("帳號或密碼錯誤")
+        .setNegativeButton("ok",null).show();
+        }
+    }
+
+    public void Close_Input_Board(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(findViewById(R.id.Sign_in_Page).getWindowToken(), 0);
     }
 }
