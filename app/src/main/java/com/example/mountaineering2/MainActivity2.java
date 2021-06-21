6cccd35b69ddc3dd06753ed2af69a04d222e7f77
Joe <joe95223@gamil.com>
2021-06-22 01:25:50 +0800

    Joe Test 0622 1:25

--- app/src/main/java/com/example/mountaineering2/MainActivity2.java
+++ app/src/main/java/com/example/mountaineering2/MainActivity2.java
@@ -11,5 +11,6 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
         Long x;
+        String test;
     }
 }
