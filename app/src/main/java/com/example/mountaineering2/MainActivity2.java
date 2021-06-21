c6cfa07cff5cdf125fe37aeddf669e2eba070738
Robert <robert31522005@yahoo.com.tw>
2021-06-22 01:28:28 +0800

    2021/06/22 01:28

--- app/src/main/java/com/example/mountaineering2/MainActivity2.java
+++ app/src/main/java/com/example/mountaineering2/MainActivity2.java
@@ -10,7 +10,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-        Long x;
-        String test;
+
     }
 }
