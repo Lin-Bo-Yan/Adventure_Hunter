9b899597aa9087841669afc03d08f2c507b6d0e7
Joe <joe95223@gamil.com>
2021-06-30 10:59:34 +0800

    0630 11:00  Update home_page.xml. Joe

--- app/src/main/java/com/example/mountaineering2/homeScreen.java
+++ app/src/main/java/com/example/mountaineering2/homeScreen.java
@@ -1,6 +1,7 @@
 package com.example.mountaineering2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -13,32 +14,20 @@ import android.widget.Toast;
 
 public class homeScreen extends AppCompatActivity {
 
-    private ImageView alishan, tataga,member_pic;
+//    private ImageView alishan, tataga,member_pic;
+    CardView member_card, mission_card, rank_card, weather_card, point_card;
     String ID;
     SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        setContentView(R.layout.home_page);
 
         sp=getApplicationContext().getSharedPreferences("MyUser", Context.MODE_PRIVATE);
 
-        alishan = (ImageView) findViewById(R.id.tataga2);
-        alishan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFindPeople();
-            }
-        });
-
-        tataga = (ImageView) findViewById(R.id.tataga);
-        tataga.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { }
-        });
-
-        member_pic=(ImageView)findViewById(R.id.member_pic);
-        member_pic.setOnClickListener(new View.OnClickListener() {
+        //會員系統
+        member_card = findViewById(R.id.member_card);
+        member_card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ID=sp.getString("ID","");
@@ -49,6 +38,70 @@ public class homeScreen extends AppCompatActivity {
                 }
             }
         });
+
+        //任務系統
+        mission_card = findViewById(R.id.mission_card);
+        mission_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              openFindPeople();
+            }
+        });
+
+        //排行系統
+        rank_card = findViewById(R.id.rank_card);
+        rank_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Open_rank();
+            }
+        });
+
+        //天氣系統
+        weather_card = findViewById(R.id.weather_card);
+        weather_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //交易系統
+        point_card = findViewById(R.id.point_card);
+        point_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+//        alishan = (ImageView) findViewById(R.id.tataga2);
+//        alishan.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openFindPeople();
+//            }
+//        });
+
+//        tataga = (ImageView) findViewById(R.id.tataga);
+//        tataga.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) { }
+//        });
+
+//        member_pic=(ImageView)findViewById(R.id.member_pic);
+//        member_pic.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ID=sp.getString("ID","");
+//                if(ID==""){
+//                    member_profile();
+//                }else {
+//                    go_member();
+//                }
+//            }
+//        });
     }
 
     //任務
@@ -81,7 +134,7 @@ public class homeScreen extends AppCompatActivity {
 
     }
 
-    public void task(View view) {
+    public void Open_rank() {
         Intent intent = new Intent(this, ShowRank.class);
         startActivity(intent);
     }
